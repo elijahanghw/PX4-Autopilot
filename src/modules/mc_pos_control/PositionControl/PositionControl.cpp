bc9ba319a2b88d16ab6cfd47ad4eb8695de18097
Elijah Ang <elijah.anghw@gmail.com>
2025-10-19 21:08:12 +0200

    Fix aux2 bug for position control

--- src/modules/mc_pos_control/PositionControl/PositionControl.cpp
+++ src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -269,32 +269,41 @@ void PositionControl::getLocalPositionSetpoint(vehicle_local_position_setpoint_s
 
 void PositionControl::getAttitudeSetpoint(vehicle_attitude_setpoint_s &attitude_setpoint, bool landed) const
 {
-	// ControlMath::thrustToAttitude(_thr_sp, _yaw_sp, attitude_setpoint);
 
 	manual_control_setpoint_s manual_control_setpoint;
 	_manual_control_setpoint_sub.copy(&manual_control_setpoint);
 
 	if (manual_control_setpoint.aux2 > 0.3f) {
+		// Tilt mode
 		ControlMath::thrustToAttitude(_thr_sp, _yaw_sp, attitude_setpoint);
 		_roll_angle = 0.f;
 		_pitch_angle = 0.f;
 
-	} else {
+	}
+
+	else {
+		// 3D thrust mode
 		if (landed) {
 			_roll_angle = 0.f;
 			_pitch_angle = 0.f;
-		} else {
+		}
+
+		else {
+			// Aux 1 set roll angle
 			// _roll_angle += _dt * manual_control_setpoint.aux1 * 2.f * M_PI_F / 2.f;
 			_roll_angle = manual_control_setpoint.aux1 * 15.f / 180.f * M_PI_F;
 		}
+
+		Quatf q_sp = Eulerf(_roll_angle, _pitch_angle, _yaw_sp);
+		q_sp.copyTo(attitude_setpoint.q_d);
+		attitude_setpoint.yaw_sp_move_rate = _yawspeed_sp;
+
+		// Rotate thrust by negative attitude
+		Dcmf att_sp_dcm{q_sp};
+		Vector3f thrust_sp_body = att_sp_dcm.transpose() * _thr_sp;
+		thrust_sp_body.copyTo(attitude_setpoint.thrust_body);
+
 	}
 
-	Quatf q_sp = Eulerf(_roll_angle, _pitch_angle, _yaw_sp);
-	q_sp.copyTo(attitude_setpoint.q_d);
-	attitude_setpoint.yaw_sp_move_rate = _yawspeed_sp;
 
-	// Rotate thrust by negative attitude
-	Dcmf att_sp_dcm{q_sp};
-	Vector3f thrust_sp_body = att_sp_dcm.transpose() * _thr_sp;
-	thrust_sp_body.copyTo(attitude_setpoint.thrust_body);
 }
